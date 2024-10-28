7e8fc2baf210a9874ab499f156090ef3295bbf59
Kowenjko <ortoswt@gmail.com>
2024-10-28 20:01:19 +0200

    create project

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	modules: ['@nuxt/fonts', '@pinia/nuxt', '@vueuse/nuxt', 'nuxt-typed-router'],
+	fonts: {
+		families: [{ name: 'Inter', provider: 'google' }],
+	},
+	pinia: {
+		storesDirs: ['./stores/**'],
+	},
+})
